974da1f9cc6e0d8593eb31a61bc10d4efd7e8748
Zoe Bryant <zoebryant@Zoes-MacBook-Pro.local>
2014-08-01 16:34:16 +0100

    Post Alex's suggestions

--- js/game.js
+++ js/game.js
@@ -1,45 +1,44 @@
 var game = new function(){
 
 	players = ["o", "x"];
-	turn = players[0];
+	next_player = players[0];
 
-	grid = ["","","","","","","","",""];
-	winning_combos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[2,5,8],[0,4,8],[2,4,6]];
+	grid = ["","","","","","","","",""]; // better way?
+	winning_combos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[2,5,8],[0,4,8],[2,4,6]]; //winning combos - dynamically?
 
 	status = '#status';
 	game_cells = '#game td';
 
 	this.init = function(){
-		$(status).text(turn + ', start the game!');
+		$(status).text(next_player + ', start the game!');
 
 		$(game_cells).click(function(){
-			cell = $(this);
-			position = $(this).attr('data-position');
+			position = $(this).attr('data-position'); // jQuery data methods
 
 			if(position != null){
-				game.take_a_turn(cell, position);
+				game.takeATurn($(this), position);
 			}
 		});
 	}
 
-	this.take_a_turn = function(cell, position){
-		game.mark_cell(cell, position);
-		game.switch_turns();
-		game.is_game_over();
+	this.takeATurn = function(cell, position){
+		game.markCell(cell, position);
+		game.switchTurns();
+		game.isGameOver();
 	}
 
-	this.mark_cell = function(cell, position){
-		$(cell).text(turn).removeAttr('data-position');
-		grid[position] = turn;
+	this.markCell = function(cell, position){
+		$(cell).text(next_player).removeAttr('data-position');
+		grid[position] = next_player;
 	}
 
-	this.switch_turns = function(){
-		players = players.reverse();
-		turn = players[0];
-		$(status).text(turn + '\'s turn');
+	this.switchTurns = function(){
+		next_player = players.reverse()[0];
+		// turn = players[0];
+		$(status).text(next_player + "'s turn");
 	}
 
-	this.is_game_over = function(){
+	this.isGameOver = function(){
 		var winner = game.won();
 		if (!winner) {
 			game.draw();
@@ -49,6 +48,7 @@ var game = new function(){
 	this.won = function(){
 		var winner = false;
 		$(winning_combos).each(function(combo_index, combo){
+			// can we refactor 2 lines below
 			winner = grid[combo[0]] == grid[combo[1]] && grid[combo[1]] == grid[combo[2]] && grid[combo[0]];
 			if(winner == players[0] || winner == players[1]){
 				game.winner(winner);
@@ -66,7 +66,7 @@ var game = new function(){
 	}
 
 	this.draw = function(){
-		is_draw = $.inArray("", grid) == -1;
+		is_draw = $.inArray("", grid) == -1; // ??
 		if(is_draw){
 			$(status).text('It was a draw!');
 			$(game_cells).removeAttr('data-position');
